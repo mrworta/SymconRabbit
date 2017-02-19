f2d2935bacc98f363314445555b80bf69e0fc03e
MrWorta <mrworta@gmail.com>
2017-02-19 01:23:00 +0100

    Working

--- SRMQ/module.php
+++ SRMQ/module.php
@@ -40,6 +40,27 @@
         */
         public function GetWork() {
 	    require __DIR__ . '/vendor/autoload.php';
+
+	    use PhpAmqpLib\Connection\AMQPStreamConnection;
+	    // use PhpAmqpLib\Message\AMQPMessage;
+
+	    $mq_srv = $this->ReadPropertyString("Server");
+	    $mq_port = $this->ReadPropertyString("Port");
+ 	    $mq_user = $this->ReadPropertyString("Username");
+ 	    $mq_pass = $this->ReadPropertyString("Password");
+
+	    $connection = new AMQPStreamConnection($mq_srv, $mq_port, $mq_user, $mq_pass);
+	    $channel = $connection->channel();
+
+	    $callback = function($msg){
+  		//echo " [x] Received ", $msg->body, "\n";
+	        //sleep(substr_count($msg->body, '.'));
+	        //echo " [x] Done", "\n";
+	        //$msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+	        return $msg->body;
+	    };
+
+	    $channel->basic_consume('symcon-alexa', '', false, true, false, false, $callback);
 	    return "Got some work";
         }
     }
