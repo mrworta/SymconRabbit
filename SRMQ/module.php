bf0195a75dd07e6a1708342782ad78aeed45ef4a
Markus Barckmann <mrworta@gmail.com>
2017-05-20 12:46:43 +0200

    Added Amazon OAUTH functions

--- SRMQ/module.php
+++ SRMQ/module.php
@@ -131,6 +131,55 @@
 	    }		    
 		    
 	}
-	    
+
+	function MsgToCall($msg) {
+	  if (is_object($msg)) {  
+  	  	$call = json_decode($msg->body);	  
+	  	if ($LogLevel > 3) { var_dump($call); }
+		return $call;
+  	  } else { if ($LogLevel > 2) { print "No object returned."; }; return null; }
+
+	}
+
+	function AmazonCheckToken($msg) {
+	   $call = MsgToCall($msg);
+           try {
+		$token = $call->user->accessToken;
+                $amazon = curl_init('https://api.amazon.com/user/profile');
+                curl_setopt($amazon, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token));
+                curl_setopt($amazon, CURLOPT_RETURNTRANSFER, true);
+
+                $result = curl_exec($amazon); curl_close($amazon);
+			    $profile = json_decode($result);								
+				
+			    return $profile->user_id == $call->AmazonAccountID;
+
+           } catch (Exception $e) {
+                IPS_LogMessage("Security", "Amazon Account validation failed.");
+		return null;
+           }
+  	}
+  
+  	function AmazonFetchProfile($msg) {
+	   $call = MsgToCall($msg);
+           try {
+ 		$token = $call->user->accessToken;
+                $amazon = curl_init('https://api.amazon.com/user/profile');
+                curl_setopt($amazon, CURLOPT_HTTPHEADER, array('Authorization: bearer ' . $token));
+                curl_setopt($amazon, CURLOPT_RETURNTRANSFER, true);
+
+                $result = curl_exec($amazon); curl_close($amazon);
+			    $profile = json_decode($result);
+				$call->AmazonAccountID  = $profile->user_id;
+				$call->AmazonAccountEMAIL = $profile->email;
+				
+		return $call;
+
+           } catch (Exception $e) {
+                IPS_LogMessage("Security", "Amazon Account validation failed.");
+		return null;
+           }
+  	}
+
     }
 ?>
